e2926ceff90f70ced25b587f7784ddb1ef999c52
an.sokolov <sokolik_85@list.ru>
2016-07-20 11:33:46 +0600

    add landing page, User model, authorisation system

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var LocalStratagy = require("passport-local");
+var flash   = require("connect-flash");
+var User   = require("./models/User");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,7 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,6 +25,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: 'anything you want!',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratagy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
+});
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var   passport = require('passport'),
+      User   = require("../models/User");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { appName: 'Eye of Providence' });
 });
+
+// AUTH ROUTES
+router.get('/register', function(req, res) {
+    res.render('register');
+});
+
+router.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            req.flash('error', err.message);
+            console.log(err);
+            return res.render('register');
+        } 
+        passport.authenticate('local')(req, res, function(){
+            req.flash('success', 'Wellcome to Eye of Providence dear ' + req.body.username);
+            res.redirect('/');
+        });
+    });
+});
+
+// show login form
+router.get('/login', function(req, res) {
+   res.render('login'); 
+});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}), function(req, res) {
+     
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.flash('success', "Successfuly log you out");
+    res.redirect('/');
+});
+
 
 module.exports = router;
