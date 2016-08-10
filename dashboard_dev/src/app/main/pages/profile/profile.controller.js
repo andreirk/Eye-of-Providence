631c03541e158b330401413bf503638a982da185
andreyirk <sokolik_85@list.ru>
2016-08-10 16:05:58 +0800

    #2306 add profile edit form

--- dashboard_dev/src/app/main/pages/profile/profile.controller.js
+++ dashboard_dev/src/app/main/pages/profile/profile.controller.js
@@ -7,14 +7,22 @@
         .controller('ProfileController', ProfileController);
 
     /** @ngInject */
-    function ProfileController(Timeline, About)
+    function ProfileController($localStorage,Timeline, About)
     {
         var vm = this;
-
+        vm.basicForm = {};
+        vm.basicForm.birthDay = '';
         // Data
         vm.posts = []; //Timeline.posts;
         vm.activities = []; //Timeline.activities;
         vm.about = About.data;
+        vm.user = $localStorage.user;
+
+        vm.basicForm.firstname = vm.user.firstName || '';
+        vm.basicForm.lastname = vm.user.lastName || '';
+        vm.basicForm.email = vm.user.email || '';
+
+       // vm.basicForm.birthDay = new Date();
         // vm.photosVideos = PhotosVideos.data;
 
         // Methods
--- dashboard_dev/src/app/main/pages/profile/profile.module.js
+++ dashboard_dev/src/app/main/pages/profile/profile.module.js
@@ -3,7 +3,7 @@
     'use strict';
 
     angular
-        .module('app.pages.profile', [])
+        .module('app.pages.profile', ['ngStorage'])
         .config(config);
 
     /** @ngInject */
